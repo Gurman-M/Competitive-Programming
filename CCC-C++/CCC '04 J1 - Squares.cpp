1f117c21db097aad5d56cb415b47ea8283faa246
Gurman Mundi <gurmanmundi10@gmail.com>
2021-11-06 20:36:08 -0400

    Create CCC '04 J1 - Squares.cpp

---
+++ CCC-C++/CCC '04 J1 - Squares.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int area;
+  cin >> area;
+  int side;
+  side = sqrt(area);
+  cout << "The largest square has side length " << side << ".\n";
+
+
+
+}
