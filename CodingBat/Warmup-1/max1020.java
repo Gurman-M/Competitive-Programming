95a955c15ed6b50cd53f2aa2d837f2fdcee0e879
Gurman Mundi <gurmanmundi10@gmail.com>
2021-11-24 19:06:08 -0500

    Create max1020.java

---
+++ CodingBat/Warmup-1/max1020.java
@@ -0,0 +1,17 @@
+public int max1020(int a, int b) {
+  if (a > b) {
+    int temp = b;
+    b = a;
+    a = temp;
+  }
+  
+  if (b >= 10 && b <= 20) {
+    return b;
+  } 
+  
+  if (a >= 10 && a <= 20) {
+    return a;
+  }
+  
+  return 0;
+}
